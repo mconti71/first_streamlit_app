80e87e6f851a5afd7b4072d8de4a2c5d49fa140d
mconti71 <38986664+mconti71@users.noreply.github.com>
2023-09-15 12:48:27 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,8 @@ streamlit.title ('My Parents New Healthy Diner')
 
 streamlit.header ('Breakfast Menu')
 
-streamlit.text (' Oatmeal 🥣 ')
-streamlit.text (' Smoothie 🥗 ')
-streamlit.text (' Hard Boiled Eggs  🐔')
-streamlit.text ('   🥑🍞 Avocado Toast')
+streamlit.text (' Oatmeal             🥣 ')
+streamlit.text (' Smoothie            🥗 ')
+streamlit.text (' Hard Boiled Eggs    🐔')
+streamlit.text (' Avocado Toast       🥑🍞')
 
